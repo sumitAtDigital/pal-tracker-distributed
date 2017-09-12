054367446ae36b46a2ed53e14ac830ec2a1c3ec4
sumitAtDigital <skmw4u@gmail.com>
2017-09-12 19:12:58 +0800

    zipkin

---
+++ platform-services/zipkin-server/src/main/java/io/pivotal/pal/tracker/zipkin/App.java
@@ -0,0 +1,15 @@
+package io.pivotal.pal.tracker.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
+import java.util.TimeZone;
+
+@SpringBootApplication
+@EnableZipkinStreamServer
+public class App {
+    public static void main(String [] args){
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        SpringApplication.run(App.class, args);
+    }
+}
